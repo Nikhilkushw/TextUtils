6bf32cb6f719a71a9f5e00014249ec91c4351760
Nikhil Repository <nikhilkush037@gmail.com>
2024-05-06 15:50:48 +0530

    my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Navbar from "./component/Navbar"
 import TextForm from "./component/TextForm"
 import About from "./component/About"
